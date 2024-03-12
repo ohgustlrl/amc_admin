ca2589d3af5eff2483dcd7e42ac410fb25610b2a
ohgustlrl <ohgustlrl@naver.com>
2024-03-13 01:44:42 +0900

    firebaseconfig 이그노어 제외처리 및 재추가

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app'
-import firebaseConfig from '@/firebaseConfig'
+import firebaseConfig from '@/plugins/firebaseConfig'
 
 
 const firebase = initializeApp(firebaseConfig)
---
+++ src/plugins/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAFbBtHzdRfUiaKux_K5YAAvjnUVMxCeb8",
+  authDomain: "stoked-coder-345512.firebaseapp.com",
+  projectId: "stoked-coder-345512",
+  storageBucket: "stoked-coder-345512.appspot.com",
+  messagingSenderId: "913296076319",
+  appId: "1:913296076319:web:7298250cc0c653e1f26f2a",
+  measurementId: "G-8DL5PMGTJ3"
+};
+
+export default firebaseConfig
