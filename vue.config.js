68776e783f06c6edce97880f29e118c7cc281557
ohgustlrl <ohgustlrl@naver.com>
2024-03-15 02:50:12 +0900

    vue 템플릿에서 es5이후 문법을 쓸수 있도록 라이브러리 설치

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  chainWebpack: (config) => {
+    config.module.rule("vue").use("vue-loader").tap((options) => {
+      options.compiler = require("vue-template-babel-compiler")
+      return options
+    })
+  },
   transpileDependencies: [
     'vuetify',
     'vuex'
