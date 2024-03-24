1f2f05c07090e7864a6c7c6890fd4a69dcae0a82
ohgustlrl <ohgustlrl@naver.com>
2024-03-25 02:46:14 +0900

    백엔드 요청값 수정

--- src/API/pubg.js
+++ src/API/pubg.js
@@ -3,8 +3,13 @@ import axios from "axios";
 const baseUrl = `http://220.117.11.123:3000/`
 
 export const getMatchesData = async (matchesInfo) => {
+  const objectToJson = JSON.stringify(matchesInfo)
   axios.post(`${baseUrl}match`, {
     // data
-    ...matchesInfo
+    ...objectToJson
+  }, {
+    headers : {
+      'Content-Type' : 'application/json'
+    }
   })
 }
