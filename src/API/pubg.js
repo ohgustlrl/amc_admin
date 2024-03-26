2fe7f4016f25b65cebb5798559d06ae4a522280b
김현석 <ohgustlrl@naver.com>
2024-03-26 10:14:02 +0900

    어카운트 아이디를 가져오는 로직을 백엔드서버에서 처리하도록 변경

--- src/API/pubg.js
+++ src/API/pubg.js
@@ -2,6 +2,11 @@ import axios from "axios";
 
 const baseUrl = `http://220.117.11.123:3000/`
 
+export const getAccntIds = async (membersName) => {
+  axios.post(`${baseUrl}accntids`, membersName)
+}
+
+
 export const getMatchesData = async (matchesInfo) => {
   const objectToJson = JSON.stringify(matchesInfo)
   axios.post(`${baseUrl}match`, {
