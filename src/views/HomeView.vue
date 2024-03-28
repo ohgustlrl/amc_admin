dd79f481a3daa86553aa0a56ab45c3c2989c070e
김현석 <ohgustlrl@naver.com>
2024-03-28 18:00:36 +0900

    홈 화면 진입 시 파이어스토어에서 멤버리스트를 가져와 store에 저장하도록 처리

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,14 +3,25 @@
 </template>
 
 <script>
+import firebase from '@/plugins/firebase'
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite'
 // @ is an alias to /src
 export default {
   name: 'HomeView',
   components: {
 
   },
+  created() {
+    this.getInitMembers();
+  },
   methods: {
-    
+    async getInitMembers() {
+      const db = getFirestore(firebase)
+      const memberCol = collection(db, 'members');
+      const memberSnapShot = await getDocs(memberCol);
+      const memebers = memberSnapShot.docs.map(doc => doc.data())
+      this.$store.commit('onMemberList', memebers)
+    }
   }
 }
 </script>
