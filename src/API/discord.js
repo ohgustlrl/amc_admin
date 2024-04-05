52b434f64ea6a93d913a82673d176f09c0816bb1
ohgustlrl <ohgustlrl@naver.com>
2024-04-05 23:37:23 +0900

    디코 api 비동기 처리후 data만 리턴하도록 처리

--- src/API/discord.js
+++ src/API/discord.js
@@ -5,7 +5,7 @@ const baseUrl = `http://220.117.11.123:3000/`
 export const getDiscordMemberListAPI = async () => {
   try {
     const result = await axios.get(`${baseUrl}dc_members`)
-    return result
+    return result.data
   } catch (error) {
     console.error(error)
   } 
