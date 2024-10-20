a8b9e267bced34f47bb2d0a59886fd895f47e9f3
ohgustlrl <ohgustlrl@naver.com>
2024-10-20 23:35:02 +0900

    대시보드 데이터 재설정

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,14 @@
   <v-container fluid :class="[isDashBoard ? 'my-0 py-0 grey lighten-5' : '']">
     <AppBar />
       <v-main>
-        <router-view :memberListProps="memberList" :mercenaryListProps="mercenaryList" :managerListProps="managerList" />
+        <router-view />
       </v-main>
     <Footer />
   </v-container>
 </template>
 
 <script>
-// import firebase from '@/plugins/firebase'
-// import { getFirestore, collection, getDocs, where, query } from 'firebase/firestore/lite'
-import {getDiscordMemberListAPI} from '../API/discord'
-import dayjs from 'dayjs'
+// import dayjs from 'dayjs'
 import AppBar from '../components/Appbar.vue'
 import Footer from '../components/Footer.vue' 
 // @ is an alias to /src
@@ -31,8 +28,6 @@ export default {
     Footer,
   },
   created() {
-    this.getDiscordMemberList()
-    // this.getInitMembers()
   },
   computed: {
     isDashBoard() {
@@ -40,144 +35,6 @@ export default {
     }
   },
   methods: {
-    // async getInitMembers() {
-    //   try {
-    //     const db = getFirestore(firebase)
-    //     const memberCol = collection(db, 'members');
-    //     const memberSnapShot = await getDocs(memberCol);
-    //     const memebers = memberSnapShot.docs.map(doc => doc.data())  
-    //     this.$store.commit('onMemberList', memebers)
-    //     this.memberList = this.$store.state.memberList
-    //   } catch (error) {
-    //     console.error(error)
-    //   }
-    // },
-
-    // async getManagerList() {
-    //   try {
-    //     const db = getFirestore(firebase)
-    //     const memberCol = collection(db, 'members');
-    //     const q = query(memberCol, where("manager", "==", "TRUE"))
-    //     const memberSnapShot = await getDocs(q);
-    //     const managerList = memberSnapShot.docs.map(doc => doc.data())
-    //     this.$store.commit('onManagerList', managerList)  
-    //   } catch (error) {
-    //     console.error(error)
-    //   }
-    // },
-
-    async getDiscordMemberList() {
-      if(this.dcMemberList !== null) {
-        this.memberList = this.$store.state.memberList
-        this.mercenaryList = this.$store.state.mercenaryList
-        this.dcMemberList = this.$store.state.dcMemberList
-      } else {
-        this.dcMemberList = await getDiscordMemberListAPI();
-        // const currentDate = new Date()
-        // const timestamp = currentDate.getTime()
-  
-        let firstData = await this.firstFomatting()
-        let {member, mercenary, managerList} = await this.secondFomtting(firstData)
-
-        this.$store.commit('onMemberList', member)
-        this.$store.commit('onMercenaryList', mercenary)
-        this.$store.commit('onManagerList', managerList)
-  
-        this.memberList = this.$store.state.memberList
-        this.mercenaryList = this.$store.state.mercenaryList
-        this.managerList = this.$store.state.managerList
-      }
-    },
-
-    async firstFomatting() {
-      let formatting = {}
-      let list = this.dcMemberList
-      
-      list.forEach(user => {
-        if(user.nickname !== null ) {
-          let userNickName = user.displayName
-          let splittedName = userNickName.split('/')
-          splittedName = splittedName.map(name => name.trim())
-          let name = splittedName[0]  
-          let sex = splittedName[1]
-          let age = splittedName[2]
-          let steamid = splittedName[3]
-          let discordid = user.userId
-          let joinedTimeStamp = user.joinedTimestamp
-          let roles = user.roles
-          let isAdmin = roles.filter(value => value == '478933666266087436')
-          let isMaster = roles.filter(value => value == '483129386204135434')
-          let admin
-          if(isAdmin.length > 0) {
-            admin = "TRUE"
-          } else {
-            admin = "FALSE"
-          }
-          let master
-          if(isMaster.length > 0) {
-            master = "TRUE"
-          } else {
-            master = "FALSE"
-          }
-          
-          formatting[user.userId] = {
-            age : age,
-            discordid : discordid,
-            name : name,
-            sex : sex,
-            steamid : steamid,
-            join : dayjs(joinedTimeStamp).format('YY.MM.DD'),
-            manager : admin,
-            master
-          }
-        }
-      });
-      return formatting
-    },
-    async secondFomtting(formatting) {
-      let list = formatting
-      let bot = []
-      let member = []
-      let mercenary = []
-      let managerList = []
-
-      for(const key in list) {
-        if(list[key].age === undefined ) {
-          bot.push( {name : list[key].name })
-        } else if (
-            list[key].steamid !== undefined && list[key].steamid.includes('지인') 
-            || 
-            list[key].steamid == undefined && list[key].sex.includes('지인')
-          ) {
-          mercenary.push({
-            discordid : list[key].discordid,
-            name : list[key].name,
-            join : list[key].join
-          }) 
-        } else if(list[key].manager === "TRUE" || list[key].master === 'TRUE') {
-          managerList.push({
-            age : list[key].age,
-            discordid : list[key].discordid,
-            name : list[key].name,
-            sex : list[key].sex,
-            steamid : list[key].steamid,
-            join : list[key].join
-          })
-        } else {
-          member.push(
-            {
-              age : list[key].age,
-              discordid : list[key].discordid,
-              name : list[key].name,
-              sex : list[key].sex,
-              steamid : list[key].steamid,
-              join : list[key].join
-            }
-          ) 
-        }   
-      }
-      return {member, mercenary, managerList}
-    },
     showLoading() {
       this.isHide = true
     },
--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -9,24 +9,61 @@
         >
           <v-card-title class="d-flex justify-space-between blue lighten-2 pa-3">
             <p class="text-h6 ma-0">클랜원 현황</p>
-            <span class="text-subtitle-2 text--secondary">업데이트 일자 {{ date }}</span>
+            <span class="text-subtitle-2 text--secondary">업데이트 일자 {{ date + ' 06:00 기준' }}</span>
           </v-card-title>
           <v-card-text height="20px" class="d-flex pa-3 justify-space-between">
             <p class="d-flex align-center ma-0">
               <v-icon color="black">mdi-account-group</v-icon>
-              <span class="text-body-1 text-right ml-2 font-weight-bold">{{ total }} 명</span>
+              <span class="text-body-1 text-right ml-2 font-weight-bold">{{ memberCount.total }} 명</span>
             </p>
             <p class="d-flex align-center ma-0">
               <v-icon color="primary">mdi-human-male</v-icon>
-              <span class="text-body-1 text-right font-weight-bold">{{ maleOfTotal }} 명</span>
+              <span class="text-body-1 text-right font-weight-bold">{{ memberCount.man }} 명</span>
             </p>
             <p class="d-flex align-center ma-0">
               <v-icon color="pink">mdi-human-female</v-icon>
-              <span class="text-body-1 text-right font-weight-bold">{{ feMaleOfTotal }} 명</span>
+              <span class="text-body-1 text-right font-weight-bold">{{ memberCount.woman }} 명</span>
             </p>
           </v-card-text>
         </v-card>
         <v-card
+          class="pa-0"
+          outlined
+          tile
+        >
+          <v-card-title class="d-flex justify-space-between blue lighten-2 pa-3">
+            <p class="text-h6 ma-0">신규 클랜원</p>
+            <span class="text-subtitle-2 text--secondary">업데이트 일자 {{ date + ' 06:00 기준' }}</span>
+          </v-card-title>
+            <template v-if="newMember.length > 0">
+              <v-card-text class="d-flex pa-3 justify-space-between" v-for="list of newMember" :key="list.id">
+                  <p class="ma-0 text-left">
+                    <span class="text-body-1 font-weight-bold mr-1">닉네임 :</span>
+                    <span class="text-body-1 font-weight-bold">{{ list.nickName }}</span>
+                  </p>
+                  <p class="ma-0 text-left">
+                    <span class="text-body-1 font-weight-bold mr-1">나이 :</span>
+                    <span class="text-body-1 font-weight-bold">{{ list.age }}</span>
+                  </p>
+                  <p class="ma-0 text-left">
+                    <span class="text-body-1 font-weight-bold mr-1">스팀아이디 :</span>
+                    <span class="text-body-1 font-weight-bold">{{ list.steamNickname }}</span>
+                  </p>
+                  <p class="ma-0 text-left">
+                    <span class="text-body-1 font-weight-bold mr-1">입장일 :</span> 
+                    <span class="text-body-1 font-weight-bold">{{ dayjs(list.joinedAt).format('YYYY.MM.DD HH:mm') }}</span>
+                  </p>
+              </v-card-text>
+            </template>
+            <template v-else>
+              <v-card-text class="d-flex pa-3 justify-space-between">
+                  <p class="ma-0">
+                    <span class="text-body-1 font-weight-bold">신규 멤버가 없습니다.</span> 
+                  </p>
+              </v-card-text>
+            </template>
+        </v-card>
+        <!-- <v-card
           class="pa-0 mb-3"
           outlined
           tile
@@ -67,48 +104,10 @@
               </span>
             </v-card-text>
           </template>
-        </v-card>
+        </v-card> -->
       </v-col>
-      <v-col md="7">
-        <v-card
-          class="pa-0"
-          outlined
-          tile
-        >
-          <v-card-title class="d-flex justify-space-between blue lighten-2 pa-3">
-            <p class="text-h6 ma-0">신규 클랜원</p>
-            <span class="text-subtitle-2 text--secondary">업데이트 일자 {{ date }}</span>
-          </v-card-title>
-          <template v-if="newMember.length > 0">
-            <v-card-text class="d-flex pa-3 justify-space-between"
-             v-for="list in newMember"
-             :key="list.name"
-            >
-                <p class="ma-0 text-left">
-                  <span class="text-body-1 font-weight-bold mr-1">닉네임 :</span>
-                  <span class="text-body-1 font-weight-bold">{{ list.name }}</span>
-                </p>
-                <p class="ma-0 text-left">
-                  <span class="text-body-1 font-weight-bold mr-1">나이 :</span>
-                  <span class="text-body-1 font-weight-bold">{{ list.age + '년생' }}</span>
-                </p>
-                <p class="ma-0 text-left">
-                  <span class="text-body-1 font-weight-bold mr-1">스팀아이디 :</span>
-                  <span class="text-body-1 font-weight-bold">{{ list.steamid }}</span>
-                </p>
-                <p class="ma-0 text-left">
-                  <span class="text-body-1 font-weight-bold mr-1">입장일 :</span> 
-                  <span class="text-body-1 font-weight-bold">{{ list.join }}</span>
-                </p>
-            </v-card-text>
-          </template>
-          <v-card-text class="d-flex pa-3 justify-space-between" v-else>
-              <p class="ma-0">
-                <span class="text-body-1 font-weight-bold">신규 멤버가 없습니다.</span> 
-              </p>
-          </v-card-text>
-        </v-card>
-      </v-col>
+      <!-- <v-col md="7">
+      </v-col> -->
     </v-row>
     <v-row class="justify-center">
       <v-col md="5">
@@ -120,75 +119,42 @@
 
 <script>
 import dayjs from 'dayjs'
+import {getDashboardApi} from '../API/member/getDashboard'
 
 export default {
   name : 'dashBoard',
-  props : ['memberListProps', 'mercenaryListProps'],
   data() {
     return {
-      date : dayjs().format('YYYY.MM.DD HH:mm:ss'),
+      dayjs,
+      date : dayjs().format('YYYY.MM.DD'),
+      memberCount : {
+        total : null,
+        man : null,
+        woman : null
+      },
+      newMember : []
     }
   },
   component() {
     dayjs
   },
   created() {
+    this.getDashboardData();
   },
   computed : {
-    total() {
-      let totlaLength = this.memberListProps.length
-      return totlaLength
-    },
-    maleOfTotal() {
-      let maleArray = []
-      this.memberListProps.map(obj => {
-        if(obj.sex == '남') {
-          maleArray.push(obj.name)
-        }
-      })
-      return maleArray.length
-    },
-    feMaleOfTotal() {
-      let feMaleArray = []
-      this.memberListProps.map(obj => {
-        if(obj.sex == '여') {
-          feMaleArray.push(obj.name)
-        }
-      })
-      return feMaleArray.length
-    },
-    visitDate() {
-      const today = dayjs();
-      return this.mercenaryListProps.map(obj => {
-        const part = obj.join.split('.').map(Number);
-        const joinDate = new Date(part[0] + 2000, part[1] - 1, part[2]);
-        const oneDay = 24 * 60 * 60 * 1000
-        const diffDays = Math.round(Math.abs((today - joinDate) / oneDay))
-        return diffDays
-      })
-    },
-    newMember() {
-      let newMember = []
-      const today = dayjs();
-      this.memberListProps.map(obj => {
-        const part = obj.join.split('.').map(Number);
-        const joinDate = new Date(part[0] + 2000, part[1] - 1, part[2]);
-        const oneDay = 24 * 60 * 60 * 1000
-        const diffDays = Math.round(Math.abs((today - joinDate) / oneDay))
-        if (diffDays <= 10) {
-          newMember.push({
-            age : obj.age,
-            name : obj.name,
-            sex : obj.sex,
-            steamid : obj.steamid,
-            join : obj.join
-          })
-        }
-      })
-      return newMember
-    },
+
   },
   methods: {
+    async getDashboardData () {
+      const result = await getDashboardApi();
+
+      console.log(result)
+
+      this.memberCount.total = result ? result.totalMembers : 0; 
+      this.memberCount.man = result ? result.maleMembers : 0;
+      this.memberCount.woman = result ? result.femaleMembers : 0;
+      this.newMember = result ? result.newMembers : [];
+    }
   }
 }
 </script>
