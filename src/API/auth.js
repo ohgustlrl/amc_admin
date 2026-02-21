53071c07a252022b109e697f966daed5422d12b0
ohgustlrl <ohgustlrl@naver.com>
2026-02-22 04:04:17 +0900

    진짜 마지막이길 바래

--- src/API/auth.js
+++ src/API/auth.js
@@ -5,7 +5,7 @@ const server_base_url = process.env.VUE_APP_SERVER_BASE_URL
 export const getAuthConfirm = async (authCode, currentRedirectUri) => {
   
   try {
-    const result = await axios.post(`${server_base_url}callback`, {
+    const result = await axios.post(`${server_base_url}api/callback`, {
       code: authCode,
       redirectUri: currentRedirectUri
     })
